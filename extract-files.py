4e515c29b8068f7d796d259920ed312fac87375c
Giovanni Ricca <giovanniricca@proton.me>
2023-11-30 22:39:07 +0100

    tissot: NOP gf_hal_test_notify_acquired_info
    * Allows up to enjoy oss goodix interface lib properly
    
    Change-Id: Id60717130c0ece053fd0656041c97678c1a75352

--- extract-files.py
+++ extract-files.py
@@ -30,6 +30,8 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('libprotobuf-cpp-lite.so'),
     'vendor/lib/libmmcamera_hdr_gb_lib.so': blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib64/libgf_hal.so': blob_fixup()
+        .sig_replace('10 03 00 D0 11 52 46 F9', '10 03 00 D0 1F 20 03 D5'),
     'vendor/lib/libmmcamera_tuning.so': blob_fixup()
         .remove_needed('libmm-qcamera.so'),
     ('vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so', 'vendor/lib64/hw/gf_fingerprint.goodix.default.so'): blob_fixup()
