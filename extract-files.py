f397a62306e8948f51986b55e9ae22ed8bb8b05b
Raya Mahardika <rayapkun@hlcyn.co>
2024-11-17 21:58:31 +0700

    tissot: Migrate to py extract utils
    Change-Id: I1ee67ba23fe2574d40d1016586bf4744d3d55082

---
+++ extract-files.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    ('product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml', 'product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml'): blob_fixup()
+        .regex_replace('/system/etc/camera/', '/vendor/etc/camera/'),
+    'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
+        .remove_needed('libprotobuf-cpp-lite.so'),
+    'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    ('vendor/lib/libmmcamera_hdr_gb_lib.so', 'vendor/lib64/libgf_algo.so', 'vendor/lib64/libgf_ca.so', 'vendor/lib64/libgf_hal.so', 'vendor/lib64/libgoodixfingerprintd_binder.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib/libmmcamera_tuning.so': blob_fixup()
+        .remove_needed('libmm-qcamera.so'),
+    'vendor/lib64/libgoodixfingerprintd_binder.so': blob_fixup()
+        .remove_needed('ld-android.so')
+        .remove_needed('libbacktrace.so')
+        .remove_needed('libunwind.so')
+        .remove_needed('libkeystore_binder.so')
+        .remove_needed('libsoftkeymasterdevice.so')
+        .remove_needed('libsoftkeymaster.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'tissot',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'msm8953-common', module.vendor
+    )
+    utils.run()
