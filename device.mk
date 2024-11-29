ecb2880baa1041bed430b5ad6670214b0bc0dd7a
Giovanni Ricca <giovanniricca@proton.me>
2024-11-29 16:21:51 +0100

    tissot: Decommonize screen density
    Change-Id: I6767f4cd66e4852335f2cac1e8062afebaaefa05

--- device.mk
+++ device.mk
@@ -9,6 +9,14 @@ $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
