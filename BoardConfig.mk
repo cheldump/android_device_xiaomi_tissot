94076cfb9c6f1ca668ad43c93850e7c340531c95
Raya Mahardika <rayapkun@hlcyn.co>
2024-12-08 06:38:00 +0000

    Revert "tissot: Commonize A/B stuff"
    This reverts commit b9e59c5956ce0c23f4bd769685af2fa3b8adbdb5.
    
    Change-Id: I02bb5a316d3599f1d231c91780ccc86c22167f40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,11 +12,20 @@ DEVICE_PATH := device/xiaomi/tissot
 # Display
 TARGET_SCREEN_DENSITY := 400
 
+# Filesystem
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG += xiaomi/tissot.config
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    system
 
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2020-05-05
